1ee28ca286ce2895ad0ed7c04fd96774a4b1420a
stiny-namake <email.escolar174@gmail.com>
2025-09-04 20:16:39 -0300

    chenged something

--- app/routers/me.py
+++ app/routers/me.py
@@ -5,9 +5,9 @@ router = APIRouter()
 @router.get("/me")
 
 def me():
-    return {"nome" : "Pedro Henrique Barboza da Silva",
-            "email" : "email.escolar174@gmail.com",
-            "curso" : "Sistemas de Informação",
-            "github" : "https://github.com/stingy-namake/",
-            "cidade" : "Brejo Santo CE",
-            "interesses" : "League of Legends, Genshin Impact, Arch Linux"}
+    return {"nome":"Pedro Henrique Barboza da Silva",
+            "email":"email.escolar174@gmail.com",
+            "curso":"Sistemas de Informação",
+            "github":"https://github.com/stingy-namake/",
+            "cidade":"Brejo Santo CE",
+            "interesses":"League of Legends, Genshin Impact, Arch Linux"}
